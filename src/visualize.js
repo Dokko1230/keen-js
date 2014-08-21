03265805f668df49271068ed2e00e5bcca4ab0c5
Sean Dokko <dokko1230@gmail.com>
2014-08-21 14:51:03 -0700

    Migrate private

--- src/visualize.js
+++ src/visualize.js
@@ -712,28 +712,6 @@
     return dataform;
   }
 
-  function _pretty_number(_input) {
-    // If it has 3 or fewer sig figs already, just return the number.
-    var input = Number(_input),
-        sciNo = input.toPrecision(3),
-        prefix = "",
-        suffixes = ["", "k", "M", "B", "T"];
-
-    if (Number(sciNo) == input && String(input).length <= 4) {
-      return String(input);
-    }
-
-    if(input >= 1 || input <= -1) {
-      if(input < 0){
-        //Pull off the negative side and stash that.
-        input = -input;
-        prefix = "-";
-      }
-      return prefix + recurse(input, 0);
-    } else {
-      return input.toPrecision(3);
-    }
-
     function recurse(input, iteration) {
       var input = String(input);
       var split = input.split(".");
@@ -778,62 +756,7 @@
       }
     }
   }
-
-  function _load_script(url, cb) {
-    var doc = document;
-    var handler;
-    var head = doc.head || doc.getElementsByTagName("head");
-
-    // loading code borrowed directly from LABjs itself
-    setTimeout(function () {
-      // check if ref is still a live node list
-      if ('item' in head) {
-        // append_to node not yet ready
-        if (!head[0]) {
-          setTimeout(arguments.callee, 25);
-          return;
-        }
-        // reassign from live node list ref to pure node ref -- avoids nasty IE bug where changes to DOM invalidate live node lists
-        head = head[0];
-      }
-      var script = doc.createElement("script"),
-      scriptdone = false;
-      script.onload = script.onreadystatechange = function () {
-        if ((script.readyState && script.readyState !== "complete" && script.readyState !== "loaded") || scriptdone) {
-          return false;
-        }
-        script.onload = script.onreadystatechange = null;
-        scriptdone = true;
-        cb();
-      };
-      script.src = url;
-      head.insertBefore(script, head.firstChild);
-    }, 0);
-
-    // required: shim for FF <= 3.5 not having document.readyState
-    if (doc.readyState === null && doc.addEventListener) {
-      doc.readyState = "loading";
-      doc.addEventListener("DOMContentLoaded", handler = function () {
-        doc.removeEventListener("DOMContentLoaded", handler, false);
-        doc.readyState = "complete";
-      }, false);
-    }
-  }
-
-  function _load_style(url, cb) {
-    var link = document.createElement('link');
-
-    link.setAttribute('rel', 'stylesheet');
-
-    link.type = 'text/css';
-
-    link.href = url;
-    cb();
-
-    document.head.appendChild(link);
-
-  }
-
+ 
   Keen.Visualization.find = function(target){
     var el, match;
     if (target) {
